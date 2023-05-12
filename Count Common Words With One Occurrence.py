090a940abd7e732d5aaae0f70980dc43f1acc026
ludsonfelipe <felipe_ludson@hotmail.com>
2023-05-11 23:12:32 -0300

    'duplicate'

--- Count Common Words With One Occurrence.py
+++ Count Common Words With One Occurrence.py
@@ -10,8 +10,3 @@ def countWords(words1: list[str], words2: list[str]) -> int:
 
     return len(set(words1).intersection(set(words2)))
 
-
-word1 = ['b', 'bbb', 'bbbb','b','a']
-word2 = ['a','b','aaa']
-
-print(countWords(words1 = word1, words2 = word2))
